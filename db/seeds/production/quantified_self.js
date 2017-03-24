d2f26b66cedc584a462e8aff02aabc2a3bb92ac9
Amz K <akintner@u.rochester.edu>
2017-03-23 18:19:08 -0600

    add production seeds file

---
+++ db/seeds/production/quantified_self.js
@@ -0,0 +1,19 @@
+exports.seed = function(knex, Promise) {
+  return knex.raw('TRUNCATE quantified_self RESTART IDENTITY')
+  .then(() => {
+    return Promise.all([
+      knex.raw(
+        'INSERT INTO quantified_self (food, created_at) VALUES (?, ?)',
+        ["{name: taco (crunchy beef), calories: 155}", new Date]
+      ),
+      knex.raw(
+        'INSERT INTO quantified_self (food, created_at) VALUES (?, ?)',
+        ["{name: banana, calories: 105}", new Date]
+      ),
+      knex.raw(
+        'INSERT INTO quantified_self (food, created_at) VALUES (?, ?)',
+        ["{name: chocolate cake, calories: 452}", new Date]
+      )
+    ]);
+  });
+};
