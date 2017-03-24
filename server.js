070479ff3368c220038bea600f4cf7592b5e6370
Amz K <akintner@u.rochester.edu>
2017-03-23 18:02:31 -0600

    update cors headers in server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const CreateController = require('./lib/controllers/food_create')
 const IndexController = require('./lib/controllers/food_index')
 const ShowController = require('./lib/controllers/food_show')
 const DeleteController = require('./lib/controllers/food_delete')
 const EditController = require('./lib/controllers/food_edit')
+
+var corsOptions = {
+  origin: '*',
+  optionSuccessStatus: 200
+}
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'quantified self'
@@ -13,6 +19,8 @@ app.locals.foods = []
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true})) //this is the syntax to include for parsing HTML forms, but we won't user this here. It allows us to parse data sent to us. 
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions))
 
 app.get('/', (request, response) => {
   response.send(`Welcome to ${app.locals.title}.`)
@@ -23,6 +31,7 @@ app.get('/api/foods/:id', ShowController.show)
 app.delete("/api/foods/:id", DeleteController.deletes) 
 app.post('/api/foods', CreateController.create)
 app.put('/api/foods/edit/:id', EditController.edit)
+
 
 // this encapsulates the listen so that it doesn't run in the test environment 
 if(!module.parent) {
